a03625207dc8b23dd05c91645bcfc2ad553ba4f2
zaxutic <zaxutic@gmail.com>
2017-08-01 17:24:08 +1000

    Update FkPyramids.py

--- FkPyramids.py
+++ FkPyramids.py
@@ -1,62 +1,60 @@
 import socket
 import datetime
 import time
+import itertools
 
 
 Host = "irc.twitch.tv"
 Port = 6667
 Nick = ""  # Twitch username
 Pass = ""  # Oauth key (twitchapps.com/tmi)
-Channel = input("Channel: ")
+Channel = ""  # Twitch channel's chat you want to join
 
-s = socket.socket()
-s.connect((Host, Port))
-s.send(bytes("PASS " + Pass + "\r\n", "UTF-8"))
-s.send(bytes("NICK " + Nick + "\r\n", "UTF-8"))
-s.send(bytes("JOIN #" + Channel + " \r\n", "UTF-8"))
+def connect():
+    global s
+    global line
+    print("CONNECTING")
+    s = socket.socket()
+    s.connect((Host, Port))
+    s.send(bytes("PASS " + Pass + "\r\n", "UTF-8"))
+    s.send(bytes("NICK " + Nick + "\r\n", "UTF-8"))
+    s.send(bytes("JOIN #" + Channel + " \r\n", "UTF-8"))
+    print("CONNECTED")
+    while True:
+        line = str(s.recv(1024))
+        if "End of /NAMES list" in line:
+            print("Entered " + Channel + "'s chat")
+            break
 
 
 def send_message(message):
     s.send(bytes("PRIVMSG #" + Channel + " :" + message + "\r\n", "UTF-8"))
 
 length = 0
-x = 0
-sec = 2
-a = 0
 
-while True:
-    line = str(s.recv(1024))
-    if "End of /NAMES list" in line:
-        print("Sucessfully joined " + Channel)
-        break
+connect()
 
 while True:
     line = s.recv(1024).decode("utf-8")
     if len(line) == 0:
-        while True:
-            try:
-                print("Reconnecting in" + str(sec) + "seconds...")
-                s.close
-                time.sleep(sec * 1000)
-                if sec != 16 and a % 2 == 1:
-                     sec * 2
-                a += 1
-                s = socket.socket()
-                s.connect((Host, Port))
-                s.send(bytes("PASS " + Pass + "\r\n", "UTF-8"))
-                s.send(bytes("NICK " + Nick + "\r\n", "UTF-8"))
-                s.send(bytes("JOIN #" + Channel + " \r\n", "UTF-8"))
-                break
-            except:
-                pass
-    else:
         sec = 2
-        a = 1
+        while True:
+            for i in itertools.count():
+                try:
+                    sys_print_write("Reconnecting in " + str(sec) + " seconds...")
+                    s.close()
+                    time.sleep(sec * 1000)
+                    if sec != 16 and i % 2 == 1:
+                        sec *= 2
+                    connect()
+                    break
+                except:
+                    pass
     parts = line.split(':')
     if line.startswith('PING'):
-        print("---RECEIVED PONG---")
+        print("RECEIVED PONG")
         s.send(bytes("PONG\r\n", "UTF-8"))
-        print("---SENT PONG---")
+        print("SENT PONG")
     elif len(parts) > 2:
         message = ':'.join(parts[2:])
         username = parts[1].split("!")[0]
@@ -65,44 +63,32 @@ while True:
         current_time = datetime.datetime.now()
         print("{:%H:%M:%S} ".format(current_time) + username + ": " + message[:-1])
 
-        if msg_parts[0] == '!pyblockchan' or msg_parts[0] == '!channel':
+        if msg_parts[0] == '!channel':
             if len(msg_parts) == 2:
-                Channel = msg_parts[1]
-                s.close
-                s = socket.socket()
-                s.connect((Host, Port))
-                s.send(bytes("PASS " + Pass + "\r\n", "UTF-8"))
-                s.send(bytes("NICK " + Nick + "\r\n", "UTF-8"))
-                s.send(bytes("JOIN #" + Channel + " \r\n", "UTF-8"))
+                send_message("Switching channel to " + Channel + "\r\n", "UTF-8")
+                print("Switching channel to " + Channel)
+                Channel = m_parts[1]
+                s.close()
+                connect()
             else:
-                send_message('Syntax: ' + msg_parts[0] + ' [channel]')
+                send_message('Syntax: ' + m_parts[0] + ' [channel]')
 
-        if username != "zaxutic" and username != "fkpyramids":
-            if len(msg_parts) == 1 + length:
+        if username != "fkpyramids":  # Put the username of the bot here
+            if len(m_parts) == 1:
+                py_part = message
+                length = 1
+            elif len(m_parts) == 1 + length:
                 length += 1
 
-                if len(msg_parts) == 1:
-                    p_part = message
-                    x = 1
-                elif x == 1:
-                    for part in msg_parts:
-                        if part != p_part:
-                            length = 0
-                            x = 0
-                else:
-                    length = 0
+                for part in m_parts:
+                    if part != py_part:
+                        length = 0
 
                 if length == 3:
                     for i in [1, 2, 3, 2, 1]:
                         send_message("no " * i)
                     length = 0
-                    x = 0
             else:
-                if len(msg_parts) == 1:
-                    p_part = message
-                    length = 1
-                else:
-                    length = 0
+                length = 0
         else:
             length = 0
-
